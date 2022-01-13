d084eadcfa3a3ff9d1f1116d4ec69b70f19c1e46
lielove <truongpv328@gmail.com>
2022-01-13 14:36:40 +0700

    commit

---
+++ src/User.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome Developer! I'm Sky Albert</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail v-bind:name="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        
+        data: function(){
+            return {
+                name: 'LieLove'
+            }
+        },
+        methods: {
+            changeName(){
+                this.name = 'Pham Van Truong';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/UserDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="component">
+        <h3>Information User Details</h3>
+        <p>List Details</p>
+        <p>My name is: {{ name }} </p>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['name']
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
